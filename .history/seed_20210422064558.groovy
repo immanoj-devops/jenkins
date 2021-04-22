7adb011b01beb95f75da9aac293cd8e1661eab1e
mpsre <mp7652@zebra.com>
2021-04-22 06:46:05 -0500

    --- Auto-Git Commit ---

--- .history/seed_20210422064558.groovy
+++ .history/seed_20210422064558.groovy
@@ -41,7 +41,7 @@ pipelineJob('frontend-ci') {
 }
 
 
-pipelineJob('shipping-ci') {
+pipelineJob('frontend-ci') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
