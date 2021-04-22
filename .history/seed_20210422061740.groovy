32b47c8d665b6c1bbabc78023977a8f7d8b7da9e
mpsre <mp7652@zebra.com>
2021-04-22 06:17:45 -0500

    --- Auto-Git Commit ---

---
+++ .history/seed_20210422061740.groovy
@@ -0,0 +1,20 @@
+pipelineJob('frontend-ci') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps54/_git/frontend.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
